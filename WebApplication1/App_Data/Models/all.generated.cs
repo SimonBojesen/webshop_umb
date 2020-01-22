4f6895566a176317c8b6d1e67466b3fcc6c52790
Alek2750 <alek2750@gmail.com>
2020-01-22 12:47:05 +0100

    merge

--- WebApplication1/App_Data/Models/all.generated.cs
+++ WebApplication1/App_Data/Models/all.generated.cs
@@ -7,7 +7,7 @@ using  Umbraco.Core.Models.PublishedContent;
 using  Umbraco.Web;
 using  Umbraco.ModelsBuilder.Embedded;
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "6054f97a4dcec818")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
